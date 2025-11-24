2e29631a63ba919ee028a8418cb21fe3654fdfaa
Isabella <isabella.juarezl111@gmail.com>
2025-11-24 12:37:38 -0600

    camera set up & new character (missing tilemap)

--- main.c
+++ main.c
@@ -1,30 +1,27 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute 'raylib_compile_execute' script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   To test the examples on Web, press F6 and execute 'raylib_compile_execute_web' script
-*   Web version of the program is generated in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2024 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
+#include "raymath.h"
+
+#define GRAVITY 400
+#define PLAYER_JUMP_SPD 350.0f
+#define PLAYER_HOR_SPD 200.0f
+
+#define MAX_ENVIRONMENT_ELEMENTS    5
+
+//----------------------------------------------------------------------------------
+// Types and Structures Definition
+//----------------------------------------------------------------------------------
+typedef struct Player {
+    Vector2 position;
+    float speed;
+    bool canJump;
+} Player;
+
+typedef struct EnvElement {
+    Rectangle rect;
+    int blocking;
+    Color color;
+} EnvElement;
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -35,55 +32,148 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib platformer");
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - automation events");
 
-    Texture2D guy = LoadTexture("resources/AnimationSheet_Character.png");
+    // Define player
+    Player player = { 0 };
+    player.position = (Vector2){ 400, 280 };
+    player.speed = 0;
+    player.canJump = false;
 
-    int frameWidth = guy.width/8;
-    int frameHeight = guy.height/9;
+    // Define environment elements (platforms)
+    EnvElement envElements[MAX_ENVIRONMENT_ELEMENTS] = {
+        {{ 0, 0, 1000, 400 }, 0, LIGHTGRAY },
+        {{ 0, 400, 1000, 200 }, 1, GRAY },
+        {{ 300, 200, 400, 10 }, 1, GRAY },
+        {{ 250, 300, 100, 10 }, 1, GRAY },
+        {{ 650, 300, 100, 10 }, 1, GRAY }
+    };
 
-    typedef struct{
-        char* name[32];
-        float fps;
-        int frameCount;
-        int current;
-    } AnimationClip;
+    // Define camera
+    Camera2D camera = { 0 };
+    camera.target = player.position;
+    camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
-    Rectangle sourceRec = { 0.0f, 0.0f, (float)frameWidth, (float)frameHeight };
-    Vector2 position = { 375.0f, 200.0f };
-    int currentFrame = 0;
-
-    int framesCounter = 0;
-    int framesSpeed = 4; 
-    
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
         // Update
         //----------------------------------------------------------------------------------
-        framesCounter++;
+        float deltaTime = 0.015f;//GetFrameTime();
 
-        if (framesCounter >= (60/framesSpeed))
+        // Dropped files logic
+        //---------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------
+
+        // Update player
+        //----------------------------------------------------------------------------------
+        if (IsKeyDown(KEY_LEFT)) player.position.x -= PLAYER_HOR_SPD*deltaTime;
+        if (IsKeyDown(KEY_RIGHT)) player.position.x += PLAYER_HOR_SPD*deltaTime;
+        if (IsKeyDown(KEY_SPACE) && player.canJump)
         {
-            framesCounter = 0;
-            currentFrame++;
-
-            if (currentFrame == 2) currentFrame = 0;
-
-            sourceRec.x = (float)currentFrame*guy.width/8;
+            player.speed = -PLAYER_JUMP_SPD;
+            player.canJump = false;
         }
+
+        int hitObstacle = 0;
+        for (int i = 0; i < MAX_ENVIRONMENT_ELEMENTS; i++)
+        {
+            EnvElement *element = &envElements[i];
+            Vector2 *p = &(player.position);
+            if (element->blocking &&
+                element->rect.x <= p->x &&
+                element->rect.x + element->rect.width >= p->x &&
+                element->rect.y >= p->y &&
+                element->rect.y <= p->y + player.speed*deltaTime)
+            {
+                hitObstacle = 1;
+                player.speed = 0.0f;
+                p->y = element->rect.y;
+            }
+        }
+
+        if (!hitObstacle)
+        {
+            player.position.y += player.speed*deltaTime;
+            player.speed += GRAVITY*deltaTime;
+            player.canJump = false;
+        }
+        else player.canJump = true;
+
+        if (IsKeyPressed(KEY_R))
+        {
+            // Reset game state
+            player.position = (Vector2){ 400, 280 };
+            player.speed = 0;
+            player.canJump = false;
+
+            camera.target = player.position;
+            camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+            camera.rotation = 0.0f;
+            camera.zoom = 1.0f;
+        }
+
+        // Update camera
+        //----------------------------------------------------------------------------------
+        camera.target = player.position;
+        camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+        float minX = 1000, minY = 1000, maxX = -1000, maxY = -1000;
+
+        // WARNING: On event replay, mouse-wheel internal value is set
+        camera.zoom += ((float)GetMouseWheelMove()*0.05f);
+        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
+        else if (camera.zoom < 0.25f) camera.zoom = 0.25f;
+
+        for (int i = 0; i < MAX_ENVIRONMENT_ELEMENTS; i++)
+        {
+            EnvElement *element = &envElements[i];
+            minX = fminf(element->rect.x, minX);
+            maxX = fmaxf(element->rect.x + element->rect.width, maxX);
+            minY = fminf(element->rect.y, minY);
+            maxY = fmaxf(element->rect.y + element->rect.height, maxY);
+        }
+
+        Vector2 max = GetWorldToScreen2D((Vector2){ maxX, maxY }, camera);
+        Vector2 min = GetWorldToScreen2D((Vector2){ minX, minY }, camera);
+
+        if (max.x < screenWidth) camera.offset.x = screenWidth - (max.x - screenWidth/2);
+        if (max.y < screenHeight) camera.offset.y = screenHeight - (max.y - screenHeight/2);
+        if (min.x > 0) camera.offset.x = screenWidth/2 - min.x;
+        if (min.y > 0) camera.offset.y = screenHeight/2 - min.y;
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
-            
-            DrawTextureRec(guy, sourceRec, position, WHITE);
+            ClearBackground(LIGHTGRAY);
+
+            BeginMode2D(camera);
+
+                // Draw environment elements
+                for (int i = 0; i < MAX_ENVIRONMENT_ELEMENTS; i++)
+                {
+                    DrawRectangleRec(envElements[i].rect, envElements[i].color);
+                }
+
+                // Draw player rectangle
+                DrawRectangleRec((Rectangle){ player.position.x - 20, player.position.y - 40, 40, 40 }, RED);
+
+            EndMode2D();
+
+            // Draw game controls
+            DrawRectangle(10, 10, 220, 90, Fade(SKYBLUE, 0.5f));
+            DrawRectangleLines(10, 10, 220, 90, Fade(BLUE, 0.8f));
+
+            DrawText("Controls:", 20, 20, 10, BLACK);
+            DrawText("- RIGHT | LEFT: Player movement", 30, 40, 10, DARKGRAY);
+            DrawText("- SPACE: Player jump", 30, 60, 10, DARKGRAY);
+            DrawText("- R: Reset game state", 30, 80, 10, DARKGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
